66e1121066391520a348ab0138b1fa1f029009ae
Luis <eilstdq@gmail.com>
2019-03-27 12:52:46 -0400

    [ADD] Datatable Widget

---
+++ web_widget_table/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "web_widget_table",
+    "cateogry": "Hidden",
+    "summary": "This widget allows to display tables using Dtatable jQeury library.",
+    "author": "Savoir-faire Linux",
+    "maintainer": "Savoir-faire Linux",
+    "website": "http://www.savoirfairelinux.com",
+    "license": "LGPL-3",
+    "version": "0.1",
+    "depends": ["web"],
+    "data": [
+        "views/assets.xml",
+    ],
+}
